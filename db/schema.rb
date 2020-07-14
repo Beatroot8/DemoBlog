58a7b4c8e160503ab3b6598931e37080bc82a8fb
George <georgeclyde@hotmail.com>
2020-07-14 18:05:29 +0100

    comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_12_142543) do
+ActiveRecord::Schema.define(version: 2020_07_14_161649) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
